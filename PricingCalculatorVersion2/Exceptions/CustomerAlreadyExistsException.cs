d44de95510ba657e0e83ad2bd3525b80ee0359d3
Oscar Andersson <oscarandersson2@gmail.com>
2021-12-13 15:00:51 +0100

    Updated Exception classes

--- PricingCalculatorVersion2/Exceptions/CustomerAlreadyExistsException.cs
+++ PricingCalculatorVersion2/Exceptions/CustomerAlreadyExistsException.cs
@@ -1,22 +1,45 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace PricingCalculator.Exceptions
 {
     /// <summary>
     /// Exception som skall kastas när man skall lägga till en customer till repository och en customer med customerId redan finns
     /// </summary>
+    [Serializable]
     public class CustomerAlreadyExistsException : Exception
     {
         /// <summary>
         /// Konstruktor
         /// </summary>
-        public CustomerAlreadyExistsException() { }
+        public CustomerAlreadyExistsException() 
+        { 
+        }
 
         /// <summary>
         /// Konstruktor
         /// </summary>
-        /// <param name="text">Text som skall visas</param>
-        public CustomerAlreadyExistsException(string text) : base(text)
-        { }
+        /// <param name="message">Text som skall visas</param>
+        public CustomerAlreadyExistsException(string message) : base(message)
+        { 
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="message">Text som skall visas</param>
+        /// <param name="innerException">Exception</param>
+        public CustomerAlreadyExistsException(string message, Exception innerException) : base(message, innerException)
+        { 
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected CustomerAlreadyExistsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
--- PricingCalculatorVersion2/Exceptions/InvalidServiceBaseCostInAppsettingsException.cs
+++ PricingCalculatorVersion2/Exceptions/InvalidServiceBaseCostInAppsettingsException.cs
@@ -1,22 +1,45 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace PricingCalculator.Exceptions
 {
     /// <summary>
     /// Exception som skall kastas när ServiceBaseCost data i Appsettings.json inte är korrekt
     /// </summary>
+    [Serializable]
     public class InvalidServiceBaseCostInAppsettingsException : Exception
     {
         /// <summary>
         /// Konstruktor
         /// </summary>
-        public InvalidServiceBaseCostInAppsettingsException() { }
+        public InvalidServiceBaseCostInAppsettingsException() 
+        { 
+        }
 
         /// <summary>
         /// Konstruktor
         /// </summary>
-        /// <param name="text">Text som skall visas</param>
-        public InvalidServiceBaseCostInAppsettingsException(string text) : base(text)
-        { }
+        /// <param name="message">Text som skall visas</param>
+        public InvalidServiceBaseCostInAppsettingsException(string message) : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="message">Text som skall visas</param>
+        /// <param name="innerException">Exception</param>
+        public InvalidServiceBaseCostInAppsettingsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected InvalidServiceBaseCostInAppsettingsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
